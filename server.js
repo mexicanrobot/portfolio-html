a6cbb20f1d4b9d193a2445f352a1630e1a825e14
Alejandro Garcia <alejandro.garcia@thermofisher.com>
2019-05-26 17:43:03 -0700

    Change default port

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use('/css',express.static(__dirname + 'public/css'));
 app.use('/js',express.static(__dirname + 'public/js'));
 app.use('/images',express.static(__dirname + 'public/images'));
 
-var server = app.listen(8081, function() {
-    var port = server.address().port;
+var port = process.env.PORT || 8081
+
+var server = app.listen(port, function() {
     console.log("Server started at http://localhost:%s",port);
 });
